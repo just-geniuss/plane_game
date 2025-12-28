bd1a0e15c478b4f7b410f58fe65245c44193f2d8
ArtefactlXXX <artyom.pervykh@icloud.com>
2025-12-28 23:04:33 +0300

    fix: I_hate_windows

--- include/systems/CollisionSystem.hpp
+++ include/systems/CollisionSystem.hpp
@@ -7,19 +7,24 @@ namespace CollisionSystem
 {
     inline bool intersects(const Entity& a, const Entity& b)
     {
-#if SFML_VERSION_MAJOR >= 3
-        return sf::intersects(a.bounds(), b.bounds());
-#else
-        return a.bounds().intersects(b.bounds());
-#endif
+        #if SFML_VERSION_MAJOR >= 3
+                const auto r1 = a.bounds();
+                const auto r2 = b.bounds();
+                return r1.left < r2.left + r2.width && r1.left + r1.width > r2.left &&
+                           r1.top < r2.top + r2.height && r1.top + r1.height > r2.top;
+        #else
+                return a.bounds().intersects(b.bounds());
+        #endif
     }
 
     inline bool intersects(const sf::FloatRect& rect, const Entity& e)
     {
-#if SFML_VERSION_MAJOR >= 3
-        return sf::intersects(rect, e.bounds());
-#else
-        return rect.intersects(e.bounds());
-#endif
+        #if SFML_VERSION_MAJOR >= 3
+                const auto r2 = e.bounds();
+                return rect.left < r2.left + r2.width && rect.left + rect.width > r2.left &&
+                           rect.top < r2.top + r2.height && rect.top + rect.height > r2.top;
+        #else
+                return rect.intersects(e.bounds());
+        #endif
     }
 }
